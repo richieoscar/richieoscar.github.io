066c65eb73f3e8ee8b6acb009ff083443bab2eba
Oscar Anyiam <oscaranyiam94@gmail.com>
2021-02-08 11:11:38 +0100

    document

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 
 let d = new Date();
             alert("Welcome to My Web Page" +"\nDate: " +d);
+
+        Math.random()
+
+
+        function range(start, end){
+            let sum = [];
+            for(let i = 0; i<= end; i++){
+                  sum[i] = start++;
+               
+            }
+            return sum.reverse;
+        }
+
+        (a, b) => 
+
+        function mul(a, b){
+           return Math.sqrt(a) * Math.sqrt(b);
+        }
+
+       
